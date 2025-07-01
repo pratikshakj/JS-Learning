be17594c20e06648b556f1df510992a57f04effb
Pratiksha K Jadhav <72095693+pratikshakj@users.noreply.github.com>
2025-07-01 21:23:16 +0530

    Add files via upload

---
+++ Conditional.js
@@ -0,0 +1,9 @@
+//Practice
+
+let age = prompt("what is your age?");
+if (age>10 && age<20){
+    console.log("Age lies between 10 and 20")
+}
+else{
+    console.log("age not in between 10 and 20")
+}
---
+++ javascript.js
@@ -0,0 +1,139 @@
+// console.log('lets learn js bitch!' );
+// let a = 56 
+// console.log(a);
+// a="prats"
+// console.log(a);
+
+
+// var a = 56 //var is globally scoped
+// var a = 'p'
+// let b ='Hello'
+// var c = null
+// var d = undefined
+
+// {
+//     let b = "bitch"
+//     console.log(b);
+// }
+
+// console.log(a);
+
+//  let b = 'Hello'
+//  let c = null
+//  let d = undefined
+// {
+//     let b = "bitch"
+//     console.log(b);
+// }
+
+// const author = "Heens"
+//  console.log(author);
+ 
+//  author= 'hello' //Throws an error bcz constant cannot be changed
+//  console.log(author);
+
+//Premitive data types in JS nn bb ss u
+// let a = null;
+// let b = 145;
+// let c = true;
+// let d = BigInt("745") + BigInt(45);
+// let e = Symbol("I'm a symbol");
+// let f = "Pratiksha";
+// let g
+
+// console.log(a, b, c, d, e, f, g);
+
+// //Object in JS
+
+// const items =
+//  {
+//     name : "Pratiksha",
+//     age : 23,
+//     "place" : false,
+//     emp_id : 1347
+// }
+
+// console.log(items);
+
+// let a = 7;
+// let b = "Hello"
+// console.log(b+a);
+
+// console.log(typeof(b+a))
+
+// const a1 = {
+//     name : 'Harry',
+//     isStudent: true
+// }
+
+// a1['friend']="Saii";
+// a1['name'] = 'Prats';
+// console.log(a1)
+
+// const dict = {
+//     hello :"Way of greetings",
+//     feckless: "ineffective; incompetent; futile",
+//     hangry: "feeling irritable or irrationally angry as a result of being hungry"
+// }
+
+// console.log(dict['hello'])
+// console.log(dict.feckless)
+
+// console.log("operators in JS");
+// let a = 45;
+// let b = 4;
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a ** b =", a ** b);
+// console.log("a % b =", a % b);
+// console.log("a++ =", a++);
+// console.log("++a =", ++a);
+// console.log("a-- =", a--);
+// console.log("--a =", --a);
+// console.log(a);
+
+// console.log("Arthematic Operators in JS")
+// let a = 8;
+// // a= a+5;
+// a+=5;
+// console.log(a)
+
+//comparison operator
+
+/*let comp1 = 6;
+let comp2 = 9;
+console.log("comp1 == comp2 is", comp1 == comp2);
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 === comp2 is", comp1 === comp2); //strict equality operator
+console.log("comp1 !== comp2 is", comp1 !== comp2); //strict non-equality operator
+console.log("comp1 < comp2 is", comp1 < comp2);
+console.log("comp1 > comp2 is", comp1 > comp2);*/
+
+//Logical operator
+
+// let a = 5;
+// let b = 6;
+
+// console.log(a<b && a==5)
+// console.log(a>b || a!=5)
+// console.log(!true)
+
+// let sum;
+// let a = prompt('enter a:');
+// let b = prompt('enter b:')
+// (a+b<4)?console.log('below'):console.log('above');
+
+// let result = (a + b > 4) ? 'above' : 'below';
+const expr = 'plans2';
+switch (expr)
+{
+   case 'plan1':
+      console.log('plan1 selected');
+      break;
+   case 'plan2':
+      console.log('plan2 selected');
+      break;
+   default:
+}
